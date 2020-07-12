e819f94635f5cad128edf8601068c5e56e625b45
SulaxanSo <sulaxan.so@live.de>
2020-07-12 19:56:20 +0200

    cleaning up the repository, uploading existing geojson to map, updating web portal

--- webapp/web-portal/map.js
+++ webapp/web-portal/map.js
@@ -1,10 +1,9 @@
-var map = L.map('map').setView([51.975, 7.61], 13);
+var map = L.map('map').setView([51.9449, 7.5724], 16);
 
 // add a OpenStreetMap layer
 var osm = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'});
         
-        osm.addTo(map);
         
 // add a humanitarian OpenStreetMap layer
 var hotOSM = new L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -22,6 +21,7 @@ var OpenTopoMap = new L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.
 var Esri_WorldImagery = new L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
+Esri_WorldImagery.addTo(map);
 
 var CartoDB_DarkMatter = new L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
@@ -36,37 +36,34 @@ var baseMaps = {
         "CartoDB_DarkMatter" : CartoDB_DarkMatter
 };
 
-var blueLayer = L.tileLayer.wms("http://10.6.1.10:8080/geoserver/uas2020_workspace/wms", {
-        layers: 'uas2020_workspace:Multispectral_2_transparent_mosaic_blue',
-        format: 'image/png',
-        transparent: true,
-        opacity: 0.7
-    });
-    map.addLayer(blueLayer);
+map.addControl(new L.Control.Fullscreen());
 
-    var greenLayer = L.tileLayer.wms("http://10.6.1.10:8080/geoserver/uas2020_workspace/wms", {
-        layers: 'uas2020_workspace:Multispectral_2_transparent_mosaic_green',
-        format: 'image/png',
-        transparent: true,
-        opacity: 0.7
-    });
-    map.addLayer(greenLayer);
+var layerControl = new L.control.layers(baseMaps).addTo(map);
 
-    var redLayer = L.tileLayer.wms("http://10.6.1.10:8080/geoserver/uas2020_workspace/wms", {
-        layers: 'uas2020_workspace:Multispectral_2_transparent_mosaic_red',
-        format: 'image/png',
-        transparent: true,
-        opacity: 0.7
-    });
-    map.addLayer(redLayer);
+var loadFile = function(event) {
+    // Init
+    var input = event.target;
+    var reader = new FileReader();
 
-    var orthomosaic = L.layerGroup([greenLayer, blueLayer, redLayer]);
+    // Invoked when file is loading. 
+    reader.onload = function(){
+        // parse content to JSON object
+        var filecontent = reader.result;
+        var geojson = JSON.parse(filecontent);
+        
+        // define the gjson object as leaflet layer
+        var parsedLayer = new L.geoJson(geojson);
 
-    var overlayMaps = {
-      "blue": blueLayer,
-      "green": greenLayer,
-      "red": redLayer
+        // add layer to map & layerControl
+        parsedLayer.addTo(map);
+        //layerControl.addOverlay(parsedLayer, name);
+        layerControl.addOverlay(parsedLayer, "geojson");
+        //zoom to added layer
+        map.fitBounds(parsedLayer.getBounds());
     };
+    
+    //Read the text
+    reader.readAsText(input.files[0]);
+   
+};
 
-// change between the layers
-L.control.layers(baseMaps, overlayMaps).addTo(map);
--- webapp/web-portal/orthomosaic.php
+++ webapp/web-portal/orthomosaic.php
@@ -1,3 +1,11 @@
+<?php
+  session_start();
+
+  if ($_SESSION['signedIn'] != 'state_signed') {
+    header('Location: http://geovm-mundus-web.uni-muenster.de/webapp-test/web-portal/signin');
+  }
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,7 +20,12 @@
 
     <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.css" rel="stylesheet">
-
+      
+  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+  <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+      <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'></script>
+<link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css' rel='stylesheet' />
+      
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -28,6 +41,14 @@
           font-size: 3.5rem;
         }
       }
+        
+        #map{height: 500px;}
+        
+        [type="file"] {
+          height: 0;
+          overflow: hidden;
+          width: 0;
+        }
     </style>
     <!-- Custom styles for this template -->
     <link href="index.css" rel="stylesheet">
@@ -38,7 +59,7 @@
   <a class="navbar-brand-two mr-0 px-3" style="color:white">Unmanned Aerial Systems for applied research - Westfälische Wilhelms-Universität Münster</a>
   <ul class="navbar-nav px-3">
     <li class="nav-item text-nowrap">
-      <a class="btn btn-primary" href="signin.html" >Sign out</a>
+        <a class="btn btn-primary" href="signin.php?action=signOut" >Sign out</a>    
     </li>
   </ul>
 </nav>
@@ -49,33 +70,33 @@
       <div class="sidebar-sticky pt-3">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="index">
               <span data-feather="home"></span>
               Home
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="orthomosaic.html">
+            <a class="nav-link active" href="orthomosaic">
               <span data-feather="layers"></span>
-              Orthomosaic
+              Orthomosaic <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="landcover.html">
+            <a class="nav-link" href="landcover">
               <span data-feather="map"></span>
               Land Cover Map
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="erosionraster.html">
+            <a class="nav-link" href="erosionraster">
               <span data-feather="grid"></span>
               Erosion Raster
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="aboutus.html">
+            <a class="nav-link" href="aboutus">
               <span data-feather="users"></span>
-              About Us <span class="sr-only">(current)</span>
+              About Us
             </a>
           </li>
         </ul>
@@ -84,16 +105,45 @@
 
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">About Us</h1>
+        <h1 class="h2">Orthomosaic</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group mr-2">
+            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+          </div>
+        </div>
+        <div>
+            <input type='file' id="file" onchange='loadFile(event)'>
+            <label for="file" class="btn btn-sm btn-outline-secondary"> 
+                <span data-feather="file"></span> 
+                Upload GeoJSON to Map
+            </label>
+        </div>
       </div>
-        <h4>Eftychia, Mirjeta and Sulaxan</h4>
-        
+
+      <p id="map"></p>
+        <div id="infocontainer" class="jumbotron">
+    <h3>More on Orthomosaics</h3>
+    <p class="lead">The orthomosaic is generated from a number of raw aerial photographs that are stitched together using appropriate geolocated images in Pix4D. The resulting image feature detailed ground surface with high spatial resolution. Preprocessing stage was carried out for 2020 RGB and Multispectral datasets that rendered products like orthomosaic, point clouds and DSM which were utilized for further analysis.
+</p>
+  </div>
     </main>
   </div>
 </div>
+<script>
+  function exportRaster() {
+    window.open("http://10.6.1.10:8080/geoserver/uas2020_workspace/wcs?service=WCS&version=1.0.0&request=GetCoverage&layers=uas2020_workspace%3AUAS2020_Multispectral_transparent_mosaic_group1&bbox=401793.96284000005%2C5755754.5486200005%2C401992.67564000003%2C5755968.106380001&width=714&height=768&format=geotiff&coverage=uas2020_workspace%3AUAS2020_Multispectral_transparent_mosaic_group1&crs=EPSG%3A32632");
+}
+</script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="../assets/dist/js/bootstrap.bundle.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-        <script src="index.js"></script></body>
+        <script src="index.js"></script>
+        <script src="map.js"></script>
+        <script src="orthomosaic.js"></script>
+
+    </body>
+    
+        
 </html>
